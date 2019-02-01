d258fa9273af2ca056247822e7f92a74cd47dfeb
JuanCasado <juan.casado@edu.uah.es>
2019-02-01 19:37:58 +0100

    MOTOR

---
+++ CODE/PRUEBAS/arduino/MOTOR/MOTOR.ino
@@ -0,0 +1,34 @@
+#include "Motor.h"
+
+Motor motor {};
+
+int v = 0;
+bool up = 1;
+bool _break = 0;
+void setup() {
+  motor.init();
+}
+
+void loop() {
+  if (up) {
+    ++v;
+    if (v == 255) {
+      up = 0;
+      _break = 1;
+    }
+  } else {
+    --v;
+    if (v == -255) {
+      up = 1;
+      _break = 1;
+    }
+  }
+  if (_break){
+    motor.setBrakes(v, v, v, v);
+    delay (500);
+    _break = 0;
+  } else {
+    motor.setSpeeds(v, v, v, v);
+    delay (10);
+  }
+}
